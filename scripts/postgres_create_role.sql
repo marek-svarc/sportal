75bb38cfe1a4e3c7360ec7063907358031661f2d
vitfo <vitfo@.....cz>
2015-06-30 14:26:04 +0200

    daoimpl - Script for creating new login role.

---
+++ scripts/postgres_create_role.sql
@@ -0,0 +1,7 @@
+﻿--revoke select, insert, update, delete on all tables in schema public from clubeek;
+--revoke usage, select on all sequences in schema public from clubeek;
+--drop role if exists clubeek;
+create role clubeek noinherit login password 'x5PurEQUVaxathe8' valid until 'infinity';
+grant select, insert, update, delete on all tables in schema public to clubeek;
+grant usage, select on all sequences in schema public to clubeek;
+
